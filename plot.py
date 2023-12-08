d0f58ae866cd1eb89e06458bf2c3fe3462685eec
Rob van Nieuwpoort <r.v.van.nieuwpoort@liacs.leidenuniv.nl>
2023-12-08 21:54:30 +0100

    plot

--- plot.py
+++ plot.py
@@ -1,1 +1,22 @@
-print ("hello")
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib as mpl
+
+# only column1 data is imported into numpy array from text file 
+sizes = np.loadtxt("test1.txt", usecols=1, skiprows=0, dtype=int) 
+times = np.loadtxt("test1.txt", usecols=4, skiprows=0, dtype=int) 
+throughput = np.loadtxt("test1.txt", usecols=7, skiprows=0, dtype=float) 
+
+print(sizes)
+print(times)
+print(throughput)
+
+
+fig, ax = plt.subplots()  # Create a figure containing a single axes.
+
+ax.set_xscale('log')
+ax.set_xlabel('message size (bytes)')
+ax.set_ylabel('throughput (MB/s)')
+
+ax.plot(sizes, throughput)
+plt.savefig("plot.pdf", format="pdf", bbox_inches="tight")
