a8c673bdf26d6af5e14fcca481c2311ad32aeb50
Rob van Nieuwpoort <r.v.van.nieuwpoort@liacs.leidenuniv.nl>
2023-12-09 17:51:26 +0100

    plot

--- plot.py
+++ plot.py
@@ -1,11 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib as mpl
+import sys
+
+
+if(len(sys.argv) < 2):
+    print("usage: python3 plot.py <filename>")
+    sys.exit(1)
+    
+filename = sys.argv[1]
 
 # only column1 data is imported into numpy array from text file 
-sizes = np.loadtxt("test1.txt", usecols=1, skiprows=0, dtype=int) 
-times = np.loadtxt("test1.txt", usecols=4, skiprows=0, dtype=int) 
-throughput = np.loadtxt("test1.txt", usecols=7, skiprows=0, dtype=float) 
+sizes = np.loadtxt(filename, usecols=1, skiprows=0, dtype=int) 
+times = np.loadtxt(filename, usecols=4, skiprows=0, dtype=int) 
+throughput = np.loadtxt(filename, usecols=7, skiprows=0, dtype=float) 
 
 fig, axs = plt.subplots(1, 2)  # Create a figure containing a single axes.
 axs[0].set_title('latency')
@@ -14,7 +22,7 @@ axs[0].set_xlabel('message size (bytes)')
 axs[0].set_yscale('log')
 axs[0].set_ylabel('latency (ns)')
 
-axs[1].set_title('thoughput')
+axs[1].set_title('throughput')
 axs[1].set_xscale('log')
 axs[1].set_xlabel('message size (bytes)')
 axs[1].set_yscale('log')
